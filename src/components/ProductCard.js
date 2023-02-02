8b0053884e04754f9d5d0582265323ded6792454
Md Sholayman <mdsholayman998@gmail.com>
2023-02-02 23:39:40 +0600

    adding cart successfully

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,33 +1,44 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../features/cart/cartSlice";
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  
   return (
-    <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
-      
-    >
-      <div className='h-52 w-52 mx-auto'>
+    <div className="shadow-lg rounded-3xl border relative  p-3 flex flex-col text-indigo-900">
+      <div className=" absolute top-2 grid place-items-center text-white right-2 bg-indigo-400 w-8 h-8 rounded-full">
+        <p>{product.quantity} </p>
+      </div>
+      <div className="h-52 w-52 mx-auto">
         <img src={product.image} alt={product.model} />
       </div>
-      <h1 className='font-bold text-center'>{product.model}</h1>
-      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
-      <div className=' flex-1'>
-        <ul className='space-y-2'>
-          {product.keyFeature.map((feature,i) => {
-            return <li className='text-sm ' key={i} >{feature}</li>;
+      <h1 className="font-bold text-center">{product.model}</h1>
+      <p className="text-center font-semibold mb-3">Rating: {product.rating}</p>
+      <div className=" flex-1">
+        <ul className="space-y-2">
+          {product.keyFeature.map((feature, i) => {
+            return (
+              <li className="text-sm " key={i}>
+                {feature}
+              </li>
+            );
           })}
         </ul>
       </div>
-      <div className='flex gap-2 mt-5'>
-        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+      <div className="flex gap-2 mt-5">
+        <button
+          className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          onClick={() => dispatch(addToCart(product))}
+        >
           Add to cart
         </button>
         <button
-          title='Add to wishlist'
-          className='bg-indigo-500  py-1 px-2 rounded-full'
+          title="Add to wishlist"
+          className="bg-indigo-500  py-1 px-2 rounded-full"
         >
-          <BiListPlus className='text-white' />
+          <BiListPlus className="text-white" />
         </button>
       </div>
     </div>
